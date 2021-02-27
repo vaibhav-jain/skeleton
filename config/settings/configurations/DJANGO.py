ec55835732fa28e4adf0f8c9d8f0b6dfe5145f4e
vaibhav-jain <vaibhav.jain3@pfizer.com>
2021-02-28 00:27:27 +0530

    Renamed MIDDLEWARE_CLASSES to MIDDLEWARE as per new django version

--- config/settings/configurations/DJANGO.py
+++ config/settings/configurations/DJANGO.py
@@ -23,14 +23,13 @@ INSTALLED_APPS = [
     'django_extensions',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
